d3c11469b4978bb40f4a9412ba1769a4722d4915
abhijai07 <abhijai07@gmail.com>
2026-05-10 22:12:39 -0400

    fix backend cors

--- backend/main.py
+++ backend/main.py
@@ -19,9 +19,10 @@ app = FastAPI(title="Fitness Tracker API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "https://fitness-tracker-1-c1f7.onrender.com",
-],
+        "https://fitness-tracker-1-c1f7.onrender.com",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
